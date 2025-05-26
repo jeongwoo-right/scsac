3c3723fff652b0d3fa94e0323e6fdc980d100283
jeongwoo-right <jeongwoocode@gmail.com>
2025-05-26 09:15:24 +0900

    Revert "editProfilePage - temp"
    This reverts commit 0875f4447797751b3691ad913451f80cfabbccc1.

--- scsac-fe/scsac-front/src/App.tsx
+++ scsac-fe/scsac-front/src/App.tsx
@@ -3,8 +3,6 @@ import Header from './components/Header'
 import LoginPage from './pages/LoginPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
-import MyPage from './pages/MyPage'
-import EditProfile from './pages/EditProfilePage'
 
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/editProfile" element={<EditProfile />} />
       </Routes>
     </BrowserRouter>
     </>
